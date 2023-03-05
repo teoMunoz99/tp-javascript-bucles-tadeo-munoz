18b0f478614cfc1dbd04a4e4641e43b8909ed80d
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-03-05 17:32:40 -0300

    realice el ejercicio 13

---
+++ js/eje13.js
@@ -0,0 +1,5 @@
+// Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let texto = window.prompt("Ingrese su texto: ");
+
+alert("Texto transformado a mayus: " + texto.toUpperCase());
