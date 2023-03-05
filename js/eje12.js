da4940096f8ac39cf6459c077f6620098795ef5f
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-03-05 17:21:03 -0300

    realice el ejercicio 12

---
+++ js/eje12.js
@@ -0,0 +1,7 @@
+// Realiza un script que genere un número aleatorio entre 1 y 99.
+
+let numero;
+
+numero = Math.random()*99;
+numero = Math.floor(numero);
+alert("El numero aleatorio entre 1 y 99 es: " + numero);
